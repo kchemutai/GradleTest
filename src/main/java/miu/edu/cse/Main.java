5e965c4ca429265db32db67d253d7098c10b4a02
Chemutai Kevin <kchemutai@gmail.com>
2024-06-03 19:09:17 -0500

    initial gradle commit

---
+++ src/main/java/miu/edu/cse/Main.java
@@ -0,0 +1,10 @@
+package miu.edu.cse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+        }
+    }
+}
